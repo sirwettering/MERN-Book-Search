c344ce512f64f90c280abdf3c70ff1e4ba1ec4d2
SirWettering <wettering1337@gmail.com>
2022-01-27 23:36:22 -0500

    updates and bug fixes

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 //import apollo server
 const { ApolloServer } = require('apollo-server-express');
 // import typeDefs and resolvers
-const { typeDefs, resolvers} = require('./schemas');
+const { typeDefs, resolvers} = require('./schema');
 const {authMiddleware} = require('./utils/auth');
 
 //db connection
